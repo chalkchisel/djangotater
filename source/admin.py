0c2e86b2414a5c476a3a96ac12dfcc5e95f390a6
Josh Ourisman <josh@joshourisman.com>
2012-09-07 13:25:41 -0400

    Added basic admin for Version.

--- source/admin.py
+++ source/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from.models import SourceFile
+from.models import Version, SourceFile
+
+
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'identifier']
 
 
 class SourceFileAdmin(admin.ModelAdmin):
@@ -8,4 +12,5 @@ class SourceFileAdmin(admin.ModelAdmin):
     list_display_links = ['file_name', ]
     list_filter = ['version', ]
 
+admin.site.register(Version, VersionAdmin)
 admin.site.register(SourceFile, SourceFileAdmin)
