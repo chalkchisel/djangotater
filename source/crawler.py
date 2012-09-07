b2ec3691ab2749cd347c2f5009b03ee39027f50e
Josh Ourisman <josh@joshourisman.com>
2012-09-07 12:38:49 -0400

    Added basic crawler to import files to database.

---
+++ source/crawler.py
@@ -0,0 +1,40 @@
+import os
+
+from .models import Version, SourceFile
+
+
+class CrawlError(Exception):
+    pass
+
+
+class SourceCrawler(object):
+    def __init__(self, identifier, root):
+        version, created = Version.objects.get_or_create(
+            identifier=identifier, defaults={'name': identifier})
+
+        if not created:
+            error_message = \
+                'A Version with identifier {} named {} already exists.'
+            raise CrawlError(error_message.format(version.identifier,
+                version.name))
+
+        self.version = version
+        self.root = root
+
+    def import_files(self, path, filenames):
+        for filename in filenames:
+            full_path = os.path.join(path, filename)
+            body = open(full_path, 'r').readlines()
+            SourceFile.objects.create(**{
+                'version': self.version,
+                'path': path,
+                'file_name': filename,
+                'body': body,
+                })
+
+    def crawl_tree(self):
+        directories = os.walk(self.root)
+        for directory in directories:
+            path = directory[0]
+            filenames = directory[2]
+            self.import_files(path, filenames)
