063347ecbbc744fd3e9becf3239646ff81b7fdbd
Josh Ourisman <josh@joshourisman.com>
2012-09-07 15:07:52 -0400

    More robust file exclusion and ignoring unencodable files.

--- source/crawler.py
+++ source/crawler.py
@@ -4,6 +4,8 @@ from pygments import highlight
 from pygments.lexers import PythonLexer
 from pygments.formatters import HtmlFormatter
 
+from django.db.utils import DatabaseError
+
 from .models import Version, SourceFile
 
 
@@ -28,21 +30,39 @@ class SourceCrawler(object):
 
         self.version = version
 
+    def valid_file(self, filename):
+        tests = [
+            lambda x: x.startswith('.'),
+            lambda x: x.endswith('.pyc'),
+            lambda x: x.endswith('.mo'),
+            lambda x: x.endswith('.gif'),
+            lambda x: x.endswith('.png'),
+        ]
+
+        for test in tests:
+            if test(filename):
+                return False
+
+        return True
+
     def import_files(self, path, filenames):
         for filename in filenames:
-            if filename.startswith('.') or filename.endswith('.pyc'):
+            if not self.valid_file(filename):
                 continue
             full_path = os.path.join(path, filename)
             with open(full_path, 'r') as f:
                 body = f.read()
             html = highlight(body, PythonLexer(), HtmlFormatter())
-            SourceFile.objects.create(**{
-                'version': self.version,
-                'path': path,
-                'file_name': filename,
-                'body': body,
-                'rendered': html,
-                })
+            try:
+                SourceFile.objects.create(**{
+                    'version': self.version,
+                    'path': path,
+                    'file_name': filename,
+                    'body': body,
+                    'rendered': html,
+                    })
+            except DatabaseError:
+                continue
 
     def crawl_tree(self):
         directories = os.walk(self.root)
